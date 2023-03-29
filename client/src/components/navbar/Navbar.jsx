cf73af3397d5880e63dc56edcba81c578e6a52d7
Jason Huang <jason97521@gmail.com>
2023-03-29 07:06:34 -0400

    send realtime notifications, like, comment, share

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,8 +2,20 @@ import "./navbar.css"
 import Notification from "../../img/notification.jsx"
 import Message from "../../img/message.svg"
 import Settings from "../../img/settings.svg"
+import { useEffect, useState } from "react"
 
-const Navbar = () => {
+const Navbar = ({socket}) => {
+  const [notifications, setNotifications] = useState([])
+
+
+  useEffect(()=>{
+    socket.on("getNotification", data=>{
+      setNotifications(prev=>[...prev, data])
+    })
+  },[socket])
+
+  console.log(notifications)
+
   return (
     <div className="navbar">
         <span className="logo">notification app</span>
