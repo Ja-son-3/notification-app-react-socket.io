d719fa85875c4f0fbfd79488fe7c6305494f31de
Jason Huang <jason97521@gmail.com>
2023-03-29 05:57:16 -0400

    card design and react like

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react"
-import "./app.css"
-import Card from "./components/card/Card"
-import Navbar from "./components/navbar/Navbar"
+import { useState } from "react";
+import "./app.css";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
+import { posts } from "./data"
 
 const App = () => {
-  const [username, setUsername] = useState("")
-  const [user, setUser] = useState("")
-
-  console.log(user)
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
 
   return (
     <div  className="container">
       {user ? (
-      <>
+        <>
         <Navbar />
-        <Card />
-        <span className="username">{user}</span>
-      </>
+        {posts.map((post) => (
+            <Card key={post.id} post={post}/>
+          ))}
+          <span className="username">{user}</span>
+        </>
       ) : (
       <div className="login">
         <input type="text" placeholder="username" onChange={(e)=>setUsername(e.target.value)}/>
--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,9 +1,34 @@
 import "./card.css"
+import Heart from "../../img/heart.svg";
+import HeartFilled from "../../img/heartFilled.svg";
+import Comment from "../../img/comment.svg";
+import Share from "../../img/share.svg";
+import Info from "../../img/info.svg";
+import { useState } from "react";
 
-const Card = () => {
+const Card = ({ post }) => {
+  const [liked, setLiked] = useState(false)
+
+  const handleNotification = () =>{
+    setLiked(true)
+  }
   return (
     <div className="card">
-        Card
+      <div className="info">
+        <img src={post.userImg} alt="" className="userImg" />
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className="postImg" />
+      <div className="interaction">
+        {liked ? (
+          <img src={HeartFilled} alt="" className="cardIcon" />
+        ) : (
+          <img src={Heart} alt="" className="cardIcon" onClick={handleNotification}/>
+        )}
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
     </div>
   )
 }
